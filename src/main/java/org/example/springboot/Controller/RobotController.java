28faf3bc80aa9623711f405ddf3f5c933619e318
zhu-wenxiang <zwx199288@163.com>
2024-12-04 07:09:44 +0800

    modified the server address

--- src/main/java/org/example/springboot/Controller/RobotController.java
+++ src/main/java/org/example/springboot/Controller/RobotController.java
@@ -1,8 +1,6 @@
 package org.example.springboot.Controller;
 
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,5 +16,10 @@ public class RobotController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @GetMapping("/")
+    public String homepage(){
+        return "this is spring boot home page";
     }
 }
